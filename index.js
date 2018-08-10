d21d3b777337276c2bc096977426f0eb18bb62fa
Samuel Manzanera <manzansa@aris-lux.lan>
2018-08-10 16:08:40 +0200

    Fix missing file

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+const schemaGenerator = require('./lib')
+
+module.exports.generateFromFile = (fileUri, { dialect } = {}) => {
+    return new Promise((resolve, reject) => {
+        if (!fileUri) {
+            return reject('File uri is required')
+        }
+        
+        fs.readFile(fileUri, 'utf8', (err, yamlSchema) => {
+            if (err) {
+                return reject(err)
+            }
+
+            const sql = schemaGenerator.generateSchema(yaml.safeLoad(yamlSchema), { dialect })
+            resolve(sql)
+        })
+    })
+}
+
+module.exports.generate = (yaml, { dialect } = {}) => {
+    const sql = schemaGenerator.generateSchema(yaml.safeLoad(yamlSchema), { dialect })
+    return sql
+}
